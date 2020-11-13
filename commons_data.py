72da19f3f7c5f908fe8efd2d62cf5e8737dd7565
luzzodev <ludovico.conti10@gmail.com>
2020-11-13 10:53:25 +0100

    Added Common Data File
    Small refactor of code.
    Aggregating static data in commons_data_file

---
+++ commons_data.py
@@ -0,0 +1,11 @@
+BIG_FILE_JSON="./../../common_corpus/nonnormalized_offersV2"
+
+N_OF_SEGMENT=8
+DIRTY_SEGMENTS_DICT="./../../common_corpus/dirty_segments/"
+CLEAN_SEGMENTS_DICT= "./../../common_corpus/clean_segments/"
+
+LOG_SEGMENTS_DICT="./../../common_corpus/logs_segments/"
+
+DEFAULT_ENCODING = "utf8"
+
+DEBUG_MODE=True
--- input_reducer.py
+++ input_reducer.py
@@ -3,21 +3,12 @@ import sys
 import json
 import time
 import math
+from commons_data import *
 
 #Due the huge size of input we consider to skip pandas usage 
 #Input Segmentation approach
 
-BIG_FILE_JSON="./../../common_corpus/nonnormalized_offersV2"
 
-N_OF_SEGMENT=8
-DIRTY_SEGMENTS_DICT="./../../common_corpus/dirty_segments/"
-CLEAN_SEGMENTS_DICT= "./../../common_corpus/clean_segments/"
-
-LOG_SEGMENTS_DICT="./../../common_corpus/logs_segments/"
-
-DEFAULT_ENCODING = "utf8"
-
-DEBUG_MODE=True
 
 
 
